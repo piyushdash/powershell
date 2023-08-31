5ed45e607ff61c5cbb20c1ba2ee74c744e43e05a
piyushdash <126020092+piyushdash@users.noreply.github.com>
2023-08-31 21:36:26 +0530

    Update monitoringReader.ps1

--- monitoringReader.ps1
+++ monitoringReader.ps1
@@ -1,1 +1,1 @@
-New-AzRoleAssignment -ObjectId 15ec27dc-28f5-4d58-93a5-951e4584c2c4 -RoleDefinitionName "Monitoring Reader" -debug
+New-AzRoleAssignment -ObjectId 15ec27dc-28f5-4d58-93a5-951e4584c2c4 -RoleDefinitionName 'Monitoring Reader' -debug
